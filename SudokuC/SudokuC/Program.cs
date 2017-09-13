73811a2b69482f9cd468481a303989908fcdeb46
Charbel Yakoub <charbel_145@hotmail.com>
2017-09-13 12:14:28 +0200

    Github test

---
+++ SudokuC/SudokuC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github test");
+        }
+    }
+}
